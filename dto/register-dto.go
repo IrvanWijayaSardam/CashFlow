1463ea79d7f18fd76289a37b1834accb767c4b44
MSI\Irvan Wijaya <irfanwijayasardam@gmail.com>
2023-06-09 15:09:45 +0700

    fix error on register

--- dto/register-dto.go
+++ dto/register-dto.go
@@ -1,13 +1,11 @@
 package dto
 
-import "mime/multipart"
-
 type RegisterDTO struct {
-	ID        uint64                `json:"id" form:"id"`
-	Name      string                `json:"name" form:"name" binding:"required"`
-	Email     string                `json:"email" form:"email" binding:"required,email"`
-	Profile   *multipart.FileHeader `form:"profile"`
-	Jk        string                `json:"jk" form:"jk" binding:"required"`
-	Pin       string                `json:"pin" form:"pin" binding:"required"`
-	Telephone string                `json:"telp" form:"telp" binding:"required"`
+	ID        uint64 `json:"id" form:"id"`
+	Name      string `json:"name" form:"name" binding:"required"`
+	Email     string `json:"email" form:"email" binding:"required,email"`
+	Profile   string `json:"profile" form:"profile"` // Changed type to string
+	Jk        string `json:"jk" form:"jk" binding:"required"`
+	Pin       string `json:"pin" form:"pin" binding:"required"`
+	Telephone string `json:"telp" form:"telp" binding:"required"`
 }
