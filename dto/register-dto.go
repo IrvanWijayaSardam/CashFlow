4250db2530f62525157744e1fbf80393c9ffad27
MSI\Irvan Wijaya <irfanwijayasardam@gmail.com>
2023-06-15 10:20:39 +0700

    fix field password not registered at dto register

--- dto/register-dto.go
+++ dto/register-dto.go
@@ -4,6 +4,7 @@ type RegisterDTO struct {
 	ID        uint64 `json:"id" form:"id"`
 	Name      string `json:"name" form:"name" binding:"required"`
 	Email     string `json:"email" form:"email" binding:"required,email"`
+	Password  string `json:"password" form:"password" binding:"required"`
 	Profile   string `json:"profile" form:"profile"` // Changed type to string
 	Jk        string `json:"jk" form:"jk" binding:"required"`
 	Pin       string `json:"pin" form:"pin" binding:"required"`
